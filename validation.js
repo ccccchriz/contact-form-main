e7f1d9c33a5177ba56d9b0f1af8d1d6be0edd0a9
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-08-09 16:29:37 +0200

    added form validation

--- validation.js
+++ validation.js
@@ -21,5 +21,48 @@ window.addEventListener("DOMContentLoaded", () => {
       lastname.classList.add("error--input");
       errorEl.classList.remove("hidden");
     }
+
+    const email = document.getElementById("email");
+    errorEl = document.getElementById("email--error");
+    document.getElementById("email--format-error").classList.add("hidden");
+    email.classList.remove("error--input");
+    errorEl.classList.add("hidden");
+    if (!email.value) {
+      const errorEl = document.getElementById("email--error");
+      email.classList.add("error--input");
+      errorEl.classList.remove("hidden");
+    } else if (!/^\S+@\S+\.\S+$/.test(email.value)) {
+      const errorEl = document.getElementById("email--format-error");
+      email.classList.add("error--input");
+      errorEl.classList.remove("hidden");
+    }
+
+    const query = document.querySelector('input[name="query"]:checked');
+    errorEl = document.getElementById("query--error");
+    errorEl.classList.add("hidden");
+    if (!query) {
+      const errorEl = document.getElementById("query--error");
+      errorEl.classList.remove("hidden");
+    }
+
+    const message = document.getElementById("message");
+    errorEl = document.getElementById("message--error");
+    message.classList.remove("error--input");
+    errorEl.classList.add("hidden");
+    if (!message.value) {
+      const errorEl = document.getElementById("message--error");
+      message.classList.add("error--input");
+      errorEl.classList.remove("hidden");
+    }
+
+    const consent = document.getElementById("consent");
+    errorEl = document.getElementById("consent--error");
+    consent.classList.remove("error--input");
+    errorEl.classList.add("hidden");
+    if (!consent.checked) {
+      const errorEl = document.getElementById("consent--error");
+      consent.classList.add("error--input");
+      errorEl.classList.remove("hidden");
+    }
   });
 });
